591a81c1a552884008ab0e41d7a3e191270c8d73
puyue <pu.yue@access-company.com>
2021-11-10 12:12:37 +0900

    add 0.8.0

---
+++ 0.8/ERC20.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token//ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    string public token_name = "token_name"; //bitcoin
+    string public token_ticker = "token_ticker"; //btc
+
+    constructor () ERC20(token_name, token_ticker) {
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+    }
+}
